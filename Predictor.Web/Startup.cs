8f6ba6dfc95f5c63ae58bdbc50ce06a22d2b4db7
Brad Lewis <bradlewis24@outlook.com>
2019-07-16 22:43:14 -0400

    Setting up the proxy server

--- Predictor.Web/Startup.cs
+++ Predictor.Web/Startup.cs
@@ -52,16 +52,6 @@ namespace Predictor.Web
                     name: "default",
                     template: "{controller}/{action=Index}/{id?}");
             });
-
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
-                }
-            });
         }
     }
 }
